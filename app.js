34e3999a4d6e8e5de88264a1ecd8cb496c4bb56f
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-16 18:10:38 +0200

    notfound

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { posts } = require("./db");
 const postsRouter = require("./routers/posts");
 const express = require("express");
 const handlerError = require("./middleware/hendolerror");
+const errorFound = require("./middleware/errorfound");
 // install express e port
 const app = express();
 const port = 3000;
@@ -17,7 +18,7 @@ app.use(postsRouter);
 
 // middleware per errore
 app.use(handlerError);
-// app.use(express.errorFind());
+app.use(errorFound);
 
 // ascolto
 app.listen(port, () => {
