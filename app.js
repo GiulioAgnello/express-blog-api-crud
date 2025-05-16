09901f15a99b88255c6992ab758707b3b6141c12
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-16 18:03:19 +0200

    generalError

--- app.js
+++ app.js
@@ -2,17 +2,22 @@
 const { posts } = require("./db");
 const postsRouter = require("./routers/posts");
 const express = require("express");
+const handlerError = require("./middleware/hendolerror");
 // install express e port
 const app = express();
 const port = 3000;
 const url = `http://localhost:${port}`;
 
-// cartella public disponibile
+// middleware
 app.use(express.static("public"));
 app.use(express.json());
 
 // accesso all routers
 app.use(postsRouter);
+
+// middleware per errore
+app.use(handlerError);
+// app.use(express.errorFind());
 
 // ascolto
 app.listen(port, () => {
